a6a1d3d67c7719e2db4df4650ae5ba7e1b48a52d
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-11 17:13:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   new_string = []
-  new_string << split_string = sentence1.reverse.split(" ")
+  new_string.unshift(sentence1.reverse.split(" "))
   new_string.each do |word| word
   puts "#{word}"
   end
